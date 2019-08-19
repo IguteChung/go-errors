2238e12fb82639fd8153544dece0b85146dc58f8
igutechung <frank310751@gmail.com>
2019-08-19 14:33:52 +0800

    fix naming

--- errors.go
+++ errors.go
@@ -2,115 +2,123 @@ package errors
 
 import "fmt"
 
-// errorTracer indicates a tracable error.
-type errorTracer interface {
-	error
+// traceable indicates an error whose stacktrace is provided.
+type traceable interface {
 	// Cause returns original error.
 	Cause() error
 	// Stack returns stack trace of this error.
 	Stack() *stack
 }
 
-// tracer implements the basic errorTracer.
-type tracer struct {
+// traceableError implements the basic traceable error.
+type traceableError struct {
 	err   error
 	stack *stack
 }
 
-func (t tracer) Error() string {
+func (t traceableError) Error() string {
 	if t.err == nil {
 		return ""
 	}
 	return t.err.Error()
 }
 
-func (t tracer) Cause() error {
+func (t traceableError) Cause() error {
 	return t.err
 }
 
-func (t tracer) Stack() *stack {
+func (t traceableError) Stack() *stack {
 	return t.stack
 }
 
 // StackTrace prints the stack trace with given error by the formatter.
-// If the error is not an errorTracer, empty string is returned.
+// If the error is not traceable, empty string is returned.
 func StackTrace(err error) string {
-	if tracer, ok := err.(errorTracer); ok {
+	if tracer, ok := err.(traceable); ok {
 		return tracer.Stack().Format()
 	}
 	return ""
 }
 
-// New creates an errorTracer with error message.
+// Cause returns the original error of a traceable error.
+// If the error is not traceable, return itself.
+func Cause(err error) error {
+	if tracer, ok := err.(traceable); ok {
+		return tracer.Cause()
+	}
+	return err
+}
+
+// New creates a traceable error with message.
 func New(msg string) error {
-	return tracer{
+	return traceableError{
 		err:   fmt.Errorf(msg),
 		stack: recordStack(),
 	}
 }
 
-// Errorf creates an errorTracer with formatted message.
-// If args contains an errorTracer, apply the stack of it.
-// If args doesn't contain an errorTracer, record the stack trace.
+// Errorf creates a traceable error with formatted message.
+// If args contains a traceable error, apply the stack of it.
+// If args doesn't contain any traceable error, record the stack trace.
 func Errorf(msg string, args ...interface{}) error {
 	e := fmt.Errorf(msg, args...)
 	for _, arg := range args {
-		if t, ok := arg.(errorTracer); ok {
-			return tracer{
+		if t, ok := arg.(traceable); ok {
+			return traceableError{
 				err:   e,
 				stack: t.Stack(),
 			}
 		}
 	}
-	return tracer{
+	return traceableError{
 		err:   e,
 		stack: recordStack(),
 	}
 }
 
-// Wrap wraps an error into an errorTracer.
-// If the wrapped error is an errorTracer, do nothing.
-// If the wrapped error is not an errorTracer, record the stack trace.
+// Wrap wraps an error into a traceable error.
+// If the wrapped error is traceable, do nothing.
+// If the wrapped error is not traceable, record the stack trace.
 func Wrap(err error) error {
-	if _, ok := err.(errorTracer); ok {
+	if _, ok := err.(traceable); ok {
 		return err
 	}
-	return tracer{
+	return traceableError{
 		err:   err,
 		stack: recordStack(),
 	}
 }
 
-// WrapMessage wraps an error into an errorTracer with message.
-// If the wrapped error is an errorTracer, only append the error message.
-// If the wrapped error is not an errorTracer, record the stack trace.
+// WrapMessage wraps an error into a traceable error with message.
+// If the wrapped error is traceable, only append the error message.
+// If the wrapped error is not traceable, record the stack trace.
 func WrapMessage(err error, msg string) error {
 	e := fmt.Errorf("%s: %v", msg, err)
-	if errorTracer, ok := err.(errorTracer); ok {
-		return tracer{
+	if tracer, ok := err.(traceable); ok {
+		return traceableError{
 			err:   e,
-			stack: errorTracer.Stack(),
+			stack: tracer.Stack(),
 		}
 	}
-	return tracer{
+	return traceableError{
 		err:   e,
 		stack: recordStack(),
 	}
 }
 
-// WrapMessagef wraps an error into an errorTracer with formatted message.
-// If the wrapped error is an errorTracer, only append the error message.
-// If the wrapped error is not an errorTracer, record the stack trace.
+// WrapMessagef wraps an error into a traceable error with formatted message.
+// If the wrapped error is traceable, only append the error message.
+// If the wrapped error is not traceable, record the stack trace.
 func WrapMessagef(err error, msg string, args ...interface{}) error {
 	formattedMsg := fmt.Sprintf(msg, args...)
 	e := fmt.Errorf("%s: %v", formattedMsg, err)
-	if errorTracer, ok := err.(errorTracer); ok {
-		return tracer{
+	if tracer, ok := err.(traceable); ok {
+		return traceableError{
 			err:   e,
-			stack: errorTracer.Stack(),
+			stack: tracer.Stack(),
 		}
 	}
-	return tracer{
+	return traceableError{
 		err:   e,
 		stack: recordStack(),
 	}
--- grpc.go
+++ grpc.go
@@ -5,13 +5,13 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-// grpcTracer implements a gRPC status error which is also an errorTracer.
-type grpcTracer struct {
-	tracer
+// grpcTraceableError implements a traceable gRPC status error.
+type grpcTraceableError struct {
+	traceableError
 }
 
 // GRPCStatus implements gRPC error.
-func (g grpcTracer) GRPCStatus() *status.Status {
+func (g grpcTraceableError) GRPCStatus() *status.Status {
 	s, ok := status.FromError(g.err)
 	if !ok {
 		return nil
@@ -19,28 +19,28 @@ func (g grpcTracer) GRPCStatus() *status.Status {
 	return s
 }
 
-// GrpcError creates an errorTracer with error message and gRPC status code.
+// GrpcError creates a traceable error with message and gRPC status code.
 func GrpcError(code codes.Code, msg string) error {
-	return grpcTracer{tracer{
+	return grpcTraceableError{traceableError{
 		err:   status.Error(code, msg),
 		stack: recordStack(),
 	}}
 }
 
-// GrpcErrorf creates an errorTracer with formatted message and gRPC status code.
-// If args contains an errorTracer, apply the stack of it.
-// If args doesn't contain an errorTracer, record the stack trace.
+// GrpcErrorf creates a traceable error with formatted message and gRPC status code.
+// If args contains a traceable error, apply the stack of it.
+// If args doesn't contain a traceable error, record the stack trace.
 func GrpcErrorf(code codes.Code, msg string, args ...interface{}) error {
 	e := status.Errorf(code, msg, args...)
 	for _, arg := range args {
-		if t, ok := arg.(errorTracer); ok {
-			return grpcTracer{tracer{
+		if t, ok := arg.(traceableError); ok {
+			return grpcTraceableError{traceableError{
 				err:   e,
 				stack: t.Stack(),
 			}}
 		}
 	}
-	return grpcTracer{tracer{
+	return grpcTraceableError{traceableError{
 		err:   e,
 		stack: recordStack(),
 	}}
